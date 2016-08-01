922c30d55cec6b58bebdb73615ecffe1384a2d03
Sojjwal Kelkar <s.kelkar@maxxton.com>
2016-08-01 18:28:12 +0530

    booking component added

--- src/services/room-booking-service.ts
+++ src/services/room-booking-service.ts
@@ -1,4 +1,5 @@
 import {Room} from '../domains/room';
+import {RoomBooking} from '../domains/room-booking';
 import {Injectable} from 'angular2/core';
 import {Http, Response, Headers} from "angular2/http";
 import {Observable} from 'rxjs/Observable';
@@ -17,5 +18,25 @@ export class RoomBookingService {
     return this.http.get(this.bookingServiceUrl).map(res => res.json());
   } 
   
+  public getBookingsForRoom(roomId:any):Observable<Array<RoomBooking>> {
+    let url = this.bookingServiceUrl + '/' + roomId + '/bookings';
+    return this.http.get(url).map(res => res.json());
+  }
   
+  public getBooking(bookingId:any):Observable<RoomBooking> {
+    let url = this.bookingServiceUrl + '/bookings/' + bookingId;
+    return this.http.get(url).map(res => res.json());
+  }
+  
+  public getRoom(roomId:any):Observable<Room> {
+    let url = this.bookingServiceUrl + '/' + roomId;
+    return this.http.get(url).map(res => res.json());
+  }
+  
+  public bookRoom(booking:RoomBooking) {
+    let url = this.bookingServiceUrl + "/" + booking.roomId + "/book";
+    let json = JSON.stringify(booking);
+    let headers = new Headers({'Content-Type': 'application/json;charset=UTF-8'});
+    return this.http.put(url, json, {headers});
+  }
 }
